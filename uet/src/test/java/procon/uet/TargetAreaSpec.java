30b362b70012e0fa55fafd2e3f03fcd69dfa2bac
dungntm58 <dungntm_58@vnu.edu.vn>
2015-07-04 01:33:41 +0700

    Amend spec to unit testing target area.

---
+++ uet/src/test/java/procon/uet/TargetAreaSpec.java
@@ -0,0 +1,114 @@
+package procon.uet;
+
+import org.junit.*;
+
+import static org.junit.Assert.*;
+
+public class TargetAreaSpec {
+	private static TargetArea targetArea;
+	private static SlatePiece s1;
+	private static SlatePiece s2;
+	
+	@Before
+	public void setup(){
+		String[] str = new String[]{
+						"00000000000000001111111111111111",
+						"00000000000000001111111111111111",
+						"01000000000000001111111111111111",
+						"00000000000000001111111111111111",
+						"00000000000000001111111111111111",
+						"00000000000000001111111111111111",
+						"00000000000000001111111111111111",
+						"00000100000000001111111111111111",
+						"00000000000000001111111111111111",
+						"00000000000000001111111111111111",
+						"00000000000000001111111111111111",
+						"00000000010000001111111111111111",
+						"11111111111111111111111111111111",
+						"11111111111111111111111111111111",
+						"11111111111111111111111111111111",
+						"11111111111111111111111111111111",
+						"11111111111111111111111111111111",
+						"11111111111111111111111111111111",
+						"11111111111111111111111111111111",
+						"11111111111111111111111111111111",
+						"11111111111111111111111111111111",
+						"11111111111111111111111111111111",
+						"11111111111111111111111111111111",
+						"11111111111111111111111111111111",
+						"11111111111111111111111111111111",
+						"11111111111111111111111111111111",
+						"11111111111111111111111111111111",
+						"11111111111111111111111111111111",
+						"11111111111111111111111111111111",
+						"11111111111111111111111111111111",
+						"11111111111111111111111111111111",
+						"11111111111111111111111111111111"
+		};
+		
+		targetArea = new TargetArea(str);
+		targetArea.commit();
+		
+		String[] s1string = new String[]{
+				"01000000",
+				"01000000",
+				"01000000",
+				"01000000",
+				"01100000",
+				"01000000",
+				"01110000",
+				"00000000"
+		};
+		String[] s2string = new String[]{
+				"00000000",
+				"01100000",
+				"01100000",
+				"01100000",
+				"01100000",
+				"00000000",
+				"00000000",
+				"00000000"
+		};
+		s1 = new SlatePiece(s1string);
+		s2 = new SlatePiece(s2string);
+	}
+	
+	@Test
+	public void testEmptyCells(){
+		assertEquals(targetArea.getEmptyCells(), 189);
+	}
+	
+	@Test
+	public void testPlaceOverlappingObstacles(){
+		assertEquals(targetArea.place(s1, 0, 0), TargetArea.PLACE_BAD);
+	}
+	
+	@Test
+	public void testPlaceOutBounds(){
+		assertEquals(targetArea.place(s1, -2, 0), TargetArea.PLACE_OUT_BOUNDS);
+	}
+	
+	@Test
+	public void testPlaceOK(){
+		assertEquals(targetArea.place(s1, 2, 0), TargetArea.PLACE_OK);
+	}
+	
+	@Test
+	public void testPlaceNonadjacent(){
+		targetArea.place(s1, 2, 0);
+		targetArea.commit();
+		assertEquals(targetArea.place(s2, 5, 1), TargetArea.PLACE_NONADJACENT);
+	}
+	
+	@Test
+	public void testOverlappingExistingPiece(){
+		targetArea.place(s1, 2, 0);
+		targetArea.commit();
+		assertEquals(targetArea.place(s2, 2, 2), TargetArea.PLACE_BAD);
+	}
+	
+	@Test
+	public void testRemoval(){
+		
+	}
+}
