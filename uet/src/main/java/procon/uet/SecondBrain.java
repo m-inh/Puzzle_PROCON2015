ffc569177448166878bf2eebfc2961549ed2c944
dungntm58 <dungntm58@vnu.edu.vn>
2015-07-09 11:38:06 +0700

    Nothing special.

--- uet/src/main/java/procon/uet/SecondBrain.java
+++ uet/src/main/java/procon/uet/SecondBrain.java
@@ -2,11 +2,19 @@ package procon.uet;
 
 import java.util.ArrayList;
 
+import procon.uet.Brain.Place;
+
 public class SecondBrain extends FirstBrain implements Brain{
 	@Override
 	public Brain.Place bestPlace(TargetArea area, SlatePiece piece){
 		ArrayList<SlatePiece> pieceArr = mostAdjacentPieces(area, piece);
 		
-		return null;
+		SlatePiece bestPiece = null;
+		//Algorithm to select the best piece
+		
+		if (bestPiece != null)
+			return new Brain.Place(bestPiece.getLocation().x, bestPiece.getLocation().y, bestPiece);
+		else
+			return new Brain.Place();
 	}
 }
